3db3da4c61fb467ca3580427df4db0a8b00158f4
Siddharth Nand <snand233@gmail.com>
2023-12-12 23:50:28 -0800

    rebased

---
+++ src/main.c
@@ -0,0 +1,40 @@
+#include "../include/io/reader.h"
+#include "../include/data/token.h"
+#include "../include/core/tokenizer.h"
+#include "../include/main.h"
+
+#include <stdio.h>
+#include <libgen.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+
+    bool isValidArguments = (argc == 2);
+
+    if (!isValidArguments) {
+        printf("Invalid arguments.\nFormat: ./main <filename>\n");
+        return 1;
+    }
+
+    char *filepath = argv[1];
+    struct USER_FILE file = get_file(filepath);
+    
+    // int num_elements_split_arr;
+    // char **split_arr = split(file.content, &num_elements_split_arr);
+    // for (size_t i = 0; i < num_elements_split_arr; i++) {
+    //     printf("%s\n", split_arr[i]);
+    // }
+
+    // struct TOKEN *tokens = tokenize(split_arr, num_elements_split_arr);
+
+    // for (size_t i = 0; i < num_elements_split_arr; i++) {
+    //     struct TOKEN token = tokens[i];
+
+    //     printf("Type: %d\n", token.type);
+    //     printf("Token: %s\n", token.value);
+    // }
+
+    // free_file(file);
+
+    return 0;
+}
