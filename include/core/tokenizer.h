ae9e82a45bc0ecbd8a24abb239bea32187f0d113
Siddharth Nand <snand233@gmail.com>
2023-12-14 17:58:52 -0800

    tokenizer returns the number of tokens

--- include/core/tokenizer.h
+++ include/core/tokenizer.h
@@ -1,7 +1,7 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
-struct TOKEN *tokenize(char **arr, int num_elements);
+struct TOKEN *tokenize(char **arr, int num_elements, int *num_tokens);
 char **split(char *content, int *num_elements);
 
 #endif
