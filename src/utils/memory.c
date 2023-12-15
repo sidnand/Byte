9a895f29bc8e0cf4eccb1191f646a37289951cc4
Siddharth Nand <snand233@gmail.com>
2023-12-14 21:50:34 -0800

    datatype check

--- src/utils/memory.c
+++ src/utils/memory.c
@@ -2,7 +2,6 @@
 #include "./../../include/utils/memory.h"
 
 void* allocate(size_t size, const char *file, int line) {
-    void* ptr = malloc(size);
-    print_error("Memory allocation failed", file, line);
+    void* ptr = malloc((size_t) size);
     return (ptr != NULL) ? ptr : ( print_error("Memory allocation failed", file, line), NULL );
 }
